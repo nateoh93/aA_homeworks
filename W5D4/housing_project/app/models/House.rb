e4f721685ca17cd6cc9764e10cf160cd0be8eb05
nateoh93 <nathanoh93@gmail.com>
2020-08-27 23:14:47 -0700

    HW submission

--- W5D4/housing_project/app/models/House.rb
+++ W5D4/housing_project/app/models/House.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: houses
+#
+#  id         :bigint           not null, primary key
+#  address    :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class House < ApplicationRecord
     validates :address, presence: true
 
